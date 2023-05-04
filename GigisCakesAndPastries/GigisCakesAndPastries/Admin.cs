e816a0191f63ef2c6b7fde4bd4fc3a943a4190ea
Rene Angelo de los Reyes <21-1-01027@vsu.edu.ph>
2023-05-04 19:35:24 +0800

    Added User and Admin Class
    Added abstract class user and admin

---
+++ GigisCakesAndPastries/GigisCakesAndPastries/Admin.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GigisCakesAndPastries
+{
+    public class Admin : User
+    {
+        public Admin(string id, string name, string email, string password, string phoneNumber, string address, DateTime birthDate, DateTime accountDateCreatead) : base (id, name, email, password, phoneNumber, address, birthDate, accountDateCreatead)
+        {
+            
+        }
+    }
+}
---
+++ GigisCakesAndPastries/GigisCakesAndPastries/User.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GigisCakesAndPastries
+{
+    public abstract class User
+    {
+        private string ID { get; set; }
+        public string Name { get; private set; }
+        public string Email { get; private set; }
+        private string Password;
+        public string PhoneNumber { get; private set; }
+        public string Address { get; private set; }
+        public DateTime BirthDate { get; private set; }
+        public DateTime AccountDateCreatead { get; private set; }
+
+        public User (string id, string name, string email, string password, string phoneNumber, string address, DateTime birthDate, DateTime accountDateCreatead)
+        {
+            this.ID = id;
+            this.Name = name;
+            this.Email = email;
+            this.Password = password;
+            this.PhoneNumber = phoneNumber;
+            this.Address = address;
+            this.BirthDate = birthDate;
+            this.AccountDateCreatead = accountDateCreatead;
+        }
+    }
+}
